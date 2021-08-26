7a2db1638c07765d58b86c98d86c8d6d67ddebbb
VladyslavFrizen <FrizenVladyslav>
2021-08-26 11:29:59 +0300

    Processing invalid CVs

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,17 @@ const app = express();
 
 app.use(fileupload());
 
+const invalidCV = {
+  isInvalid: true,
+  name: "No Name",
+  sections: {
+    skills: [],
+    education: [],
+    experience: [],
+    profile: ["Location unknown", "Working unknown", "Location unknown"],
+  },
+};
+
 app.post("/dev/cv-parser", async (req, res) => {
   try {
     let uploadedFiles = Object.values(req.files);
@@ -18,10 +29,20 @@ app.post("/dev/cv-parser", async (req, res) => {
 
     let result = {};
     const resultPromises = files.map(async (file) => {
-      const res = await parseCv(file.data);
-      result[file.name] = res;
+      try {
+        const res = await parseCv(file.data);
+        result[file.name] = res;
+      } catch (e) {
+        result[file.name] = Object.assign({}, invalidCV);
+      }
     });
     await Promise.all(resultPromises);
+
+    Object.keys(result).forEach((key) => {
+      const cv = result[key];
+      if (!cv.name) result[key] = Object.assign({}, invalidCV);
+    });
+
     res.json(result);
   } catch (e) {
     res.send(e.message || "Something went wrong");
